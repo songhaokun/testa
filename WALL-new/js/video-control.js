2acb5f71b95050ed15d7baf8042b525847a3dbfc
xiongwei <3552725372@qq.com>
2019-04-29 19:21:47 +0800

    屏保上传

---
+++ WALL-new/js/video-control.js
@@ -0,0 +1,25 @@
+var ele = document.getElementById("videobox");
+
+var video = document.getElementById("video");
+
+var dispalyAttr = ele.style.display;
+
+var cur = 0;
+
+var wait = 5 * 60;  //页面停留的时间，超过该时间则出现屏保 5 * 60  就是 5分钟
+
+document.addEventListener("mousemove", function() {
+  cur = 0;
+  if(dispalyAttr === "block") {
+    ele.style.display = "none";
+  }
+}, false)
+
+setInterval(function() {
+  cur++;
+  if(cur > wait && ele.style.display === "none") {
+    cur = 0;
+    ele.style.display = "block";
+    ele.load();
+  }
+},1000)
