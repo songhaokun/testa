a3789a47c5158168d615d58b7cb4ca072d4acc02
xiongwei <2931419357@qq.com>
2019-05-05 22:43:11 +0800

    屏保问题Bug修复

--- WALL-new/js/video-control.js
+++ WALL-new/js/video-control.js
@@ -6,12 +6,13 @@ var dispalyAttr = ele.style.display;
 
 var cur = 0;
 
-var wait = 5 * 60;  //页面停留的时间，超过该时间则出现屏保 5 * 60  就是 5分钟
+var wait = 60;  //页面停留的时间，超过该时间则出现屏保 5 * 60  就是 5分钟
 
 document.addEventListener("mousemove", function() {
   cur = 0;
   if(dispalyAttr === "block") {
     ele.style.display = "none";
+    video.pause();
   }
 }, false)
 
@@ -20,6 +21,6 @@ setInterval(function() {
   if(cur > wait && ele.style.display === "none") {
     cur = 0;
     ele.style.display = "block";
-    ele.load();
+    video.load();
   }
 },1000)
